98c43473ce3a65bd54a09b3a3f7ba29f5b9eee41
Abhishekjain-cloudeq <abhishek.jain@cloudeq.com>
2022-12-27 21:38:27 +0530

    First commit

--- Timer-trigger-function/__init__.py
+++ Timer-trigger-function/__init__.py
@@ -8,6 +8,5 @@ def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
-    print("I am laerning azure timer trigger function")
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
